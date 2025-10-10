694a7745365ee3c23f4c3455a5ace82c23cead95
Fariha Promi <farihapromi2611@gmail.com>
2025-10-10 18:37:16 +0600

    Wrap corwith in manager.Use

--- backend/cmd/serve.go
+++ backend/cmd/serve.go
@@ -10,13 +10,14 @@ import (
 func Serve() {
 	mux := http.NewServeMux()
 	manager := middleware.NewManager()
+	manager.Use(middleware.Logger, middleware.CorsWithPreflight)
 	initRoutes(mux, manager)
 
 	fmt.Println("Server running on :8080")
 
 	// Use cors-enabled globalRouter
-	globalRouter := middleware.CorsWithPreflight(mux)
-	err := http.ListenAndServe(":8080", globalRouter)
+	// globalRouter := middleware.CorsWithPreflight(mux)
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		fmt.Println("Error starting the server", err)
 	}
