c8fba7cb7a1d43c06e3de0926181762acdbc1b05
Fariha Promi <farihapromi2611@gmail.com>
2025-10-10 19:52:24 +0600

    add advance mdidleware

--- backend/cmd/serve.go
+++ backend/cmd/serve.go
@@ -13,13 +13,22 @@ func Serve() {
 	// manager.Use(middleware.Logger, middleware.CorsWithPreflight)
 	mux := http.NewServeMux()
 	initRoutes(mux, manager)
+	// globalMiddlewares := []middleware.Middleware{
+	// 	middleware.CorsWithPreflight, middleware.Logger,
+	// }m
+	manager.Use(
+		middleware.CorsWithPreflight, middleware.Logger,
+	)
+
+	wrappedMux := manager.WrapMux(mux)
 
 	fmt.Println("Server running on :8080")
 
 	// Use cors-enabled globalRouter
 	// globalRouter := middleware.CorsWithPreflight(mux)
 	//CorsWithPreflight(hudai(logger(mux)))
-	wrappedMux := manager.WrapMux(mux, middleware.Logger, middleware.CorsWithPreflight)
+	// wrappedMux := manager.WrapMux(mux, middleware.Logger, middleware.CorsWithPreflight)
+
 	err := http.ListenAndServe(":8080", wrappedMux)
 	if err != nil {
 		fmt.Println("Error starting the server", err)
--- backend/middleware/manager.go
+++ backend/middleware/manager.go
@@ -18,7 +18,7 @@ func (mngr *Manager) Use(middlewares ...Middleware) {
 	mngr.globalMiddlewares = append(mngr.globalMiddlewares, middlewares...)
 
 }
-func (mngr *Manager) WrapMux(handler http.Handler, middlewares ...Middleware) http.Handler {
+func (mngr *Manager) With(middlewares []Middleware, handler http.Handler) http.Handler {
 
 	h := handler
 	//middlewares =[hudai,logger] [0,1]
@@ -33,9 +33,31 @@ func (mngr *Manager) WrapMux(handler http.Handler, middlewares ...Middleware) ht
 		h = middleware(h)
 
 	}
-	for _, globalMiddleware := range mngr.globalMiddlewares {
-		h = globalMiddleware(h)
+	// for _, globalMiddleware := range mngr.globalMiddlewares {
+	// 	h = globalMiddleware(h)
+	// }
+
+	return h
+
+}
+func (mngr *Manager) WrapMux(handler http.Handler) http.Handler {
+
+	h := handler
+	//middlewares =[hudai,logger] [0,1]
+	// middleware.Logger http.HandlerFunc(handlers.GetProducts)))
+	// for i := len(middlewares) - 1; i >= 0; i-- {
+	// 	middleware := middlewares[i]
+	// 	n = middleware(n)
+
+	// }
+	//h=corsWithPrefligh(logger(Mux))
+	for _, middleware := range mngr.globalMiddlewares {
+		h = middleware(h)
+
 	}
+	// for _, globalMiddleware := range mngr.globalMiddlewares {
+	// 	h = globalMiddleware(h)
+	// }
 
 	return h
 
