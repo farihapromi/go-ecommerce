5f245ec55d8040a4a2bd19e17eb8a53dd0fd0ee8
Fariha Promi <farihapromi2611@gmail.com>
2025-10-10 20:01:01 +0600

    Add router as mux

--- backend/cmd/serve.go
+++ backend/cmd/serve.go
@@ -10,26 +10,17 @@ import (
 func Serve() {
 
 	manager := middleware.NewManager()
-	// manager.Use(middleware.Logger, middleware.CorsWithPreflight)
-	mux := http.NewServeMux()
-	initRoutes(mux, manager)
-	// globalMiddlewares := []middleware.Middleware{
-	// 	middleware.CorsWithPreflight, middleware.Logger,
-	// }m
+
 	manager.Use(
 		middleware.Cors,
 		middleware.Preflight,
 		middleware.Logger,
 	)
-
+	mux := http.NewServeMux()
+	initRoutes(mux, manager)
 	wrappedMux := manager.WrapMux(mux)
 
 	fmt.Println("Server running on :8080")
-
-	// Use cors-enabled globalRouter
-	// globalRouter := middleware.CorsWithPreflight(mux)
-	//CorsWithPreflight(hudai(logger(mux)))
-	// wrappedMux := manager.WrapMux(mux, middleware.Logger, middleware.CorsWithPreflight)
 
 	err := http.ListenAndServe(":8080", wrappedMux)
 	if err != nil {
