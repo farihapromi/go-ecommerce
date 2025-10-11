435ea5d913393d29c3c2acd550f51e292ac6c11a
Fariha Promi <farihapromi2611@gmail.com>
2025-10-11 12:13:18 +0600

    Load env variable thorugh config in main

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"ecommerce/cmd"
+	"ecommerce/config"
+	"fmt"
 )
 
 func main() {
+	cnf := config.GetConfig()
+	fmt.Println(cnf.HttpPort)
+	fmt.Println(cnf.ServiceName)
 	cmd.Serve()
 }
