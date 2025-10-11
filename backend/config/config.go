e073c1dcde006ca8617a4dec441c5c166fae8730
Fariha Promi <farihapromi2611@gmail.com>
2025-10-11 11:46:53 +0600

    Configure servicename and version

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,7 +1,26 @@
 package config
 
+import (
+	"fmt"
+	"os"
+)
+
 type Config struct {
 	Version     string
 	ServiceName string
 	HttpPort    int
+}
+
+func LoadConfig() {
+	version := os.Getenv("VERSION")
+	if version == "" {
+		fmt.Println("version is required")
+		os.Exit(1)
+	}
+	serviceName := os.Getenv("SERVICE_NAME")
+	if serviceName == "" {
+		fmt.Println("vSevice name is required")
+
+	}
+
 }
