9d0942e01c2f4aef285f1f4a719a1fdd5beadcd8
Fariha Promi <farihapromi2611@gmail.com>
2025-10-27 22:02:10 +0600

    Add productRepo

--- backend/repo/product.go
+++ backend/repo/product.go
@@ -1,9 +1,22 @@
 package repo
 
+type Product struct {
+	/*Struct tags are special metadata you can attach to struct fields.
+	They are written in backticks (`...`) after the field definition.*/
+	ID          int     `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	ImgUrl      string  `json:"imgUrl"`
+}
+
 type ProductRepo interface {
 	Create()
 	Get()
 	List()
 	Delete()
 	Update()
+}
+type productRepo struct {
+	productList []Product
 }
