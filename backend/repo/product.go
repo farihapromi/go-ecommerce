2c3f771cf8bea35683d4be4c2ac79dc7ce230dd9
Fariha Promi <farihapromi2611@gmail.com>
2025-11-09 11:09:13 +0600

    Add generateInitialProducts function

--- backend/repo/product.go
+++ backend/repo/product.go
@@ -21,19 +21,76 @@ type productRepo struct {
 	productList []Product
 }
 
-func NewProductRepo() productRepo {
-	return productRepo{}
+func NewProductRepo() ProductRepo {
+	repo := &productRepo{}
+	generateInitialProducts(repo)
+	return repo
 
 }
-func (r productRepo) Create() {
+func (r *productRepo) Create() {
 
 }
-func (r productRepo) Get() {
+func (r *productRepo) Get() {
 
 }
-func (r productRepo) Delete() {
+func (r *productRepo) List() {
 
 }
-func (r productRepo) Update() {
+func (r *productRepo) Delete() {
 
+}
+func (r *productRepo) Update() {
+
+}
+
+func generateInitialProducts(r *productRepo) {
+	prd1 := Product{
+		ID:          1,
+		Title:       "Orange",
+		Description: "Orange is sweet and juicy",
+		Price:       100,
+		ImgUrl:      "https://images.othoba.com/images/thumbs/0040450_orange-1-kg.jpeg",
+	}
+
+	prd2 := Product{
+		ID:          2,
+		Title:       "Mango",
+		Description: "Mango is juicy and delicious",
+		Price:       300,
+		ImgUrl:      "https://dookan.com/cdn/shop/files/Dookan_Kesar_Mangoes_82bf0570-50bf-4f04-97b4-b1b415ebc733.png?v=1724714757&width=823",
+	}
+
+	prd3 := Product{
+		ID:          3,
+		Title:       "Apple",
+		Description: "Red apple, crispy and sweet",
+		Price:       200,
+		ImgUrl:      "https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg",
+	}
+
+	prd4 := Product{
+		ID:          4,
+		Title:       "Banana",
+		Description: "Fresh yellow bananas, rich in potassium",
+		Price:       50,
+		ImgUrl:      "https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg",
+	}
+
+	prd5 := Product{
+		ID:          5,
+		Title:       "Pineapple",
+		Description: "Fresh pineapple, tropical and juicy",
+		Price:       250,
+		ImgUrl:      "https://upload.wikimedia.org/wikipedia/commons/c/cb/Pineapple_and_cross_section.jpg",
+	}
+	// Append all product.Products
+	r.productList = append(r.productList, prd1)
+	r.productList = append(r.productList, prd2)
+	r.productList = append(r.productList, prd3)
+	r.productList = append(r.productList, prd4)
+	r.productList = append(r.productList, prd5)
+
+	// product.productList = append(productList, prd1, prd2, prd3, prd4, prd5, prd6)
+
+	// fmt.Println(productList)
 }
